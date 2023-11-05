74051262e8e693aefe86c9fad77f08d2bda6f65e
Jonilson Sousa <jonilsonds9@gmail.com>
2023-11-04 22:00:26 -0300

    Adicionando funcao lambda com deploy

---
+++ README.md
@@ -0,0 +1,3 @@
+# Authorize Lambda
+
+Este é um autorizador Lambda feito em NodeJS para verificar tokens de acesso JWT.
---
+++ index.js
@@ -0,0 +1,52 @@
+const jwt = require("jsonwebtoken");
+
+function generateAuthResponse(principalId, effect, methodArn) {
+  const policyDocument = generatePolicyDocument(effect, methodArn);
+
+  return {
+    principalId,
+    policyDocument
+  };
+}
+
+function generatePolicyDocument(effect, methodArn) {
+  if (!effect || !methodArn) return null;
+
+  const policyDocument = {
+    Version: '2012-10-17',
+    Statement: [
+      {
+        Action: 'execute-api:Invoke',
+        Effect: effect,
+        Resource: methodArn
+      }
+    ]
+  };
+
+  return policyDocument;
+}
+
+function extractTokenFromHeader(event) {
+  if (event.authorizationToken && event.authorizationToken.split(' ')[0] === 'Bearer') {
+    return event.authorizationToken.split(' ')[1];
+  } else {
+    return event.authorizationToken;
+  }
+}
+
+exports.handler = (event, context, callback) => {
+  const token = extractTokenFromHeader(event) || '';
+  const methodArn = event.methodArn;
+
+  if (!token || !methodArn) return callback(null, 'Unauthorized');
+
+  const secret = Buffer.from(process.env.JWT_SECRET, 'base64');
+
+  const decoded = jwt.verify(token, secret);
+
+  if (decoded && decoded.id) {
+    return callback(null, generateAuthResponse(decoded.id, 'Allow', methodArn));
+  } else {
+    return callback(null, generateAuthResponse(decoded.id, 'Deny', methodArn));
+  }
+}
